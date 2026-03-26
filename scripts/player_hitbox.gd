b075f9124d331cfdeaa556c352c786e43c4ced2c
Eunech-ballist23 <gianroviks.com>
2026-03-26 08:43:19 +0800

    new updates for bosses

--- scripts/player_hitbox.gd
+++ scripts/player_hitbox.gd
@@ -1,6 +1,9 @@
 extends Area2D
-@export var damage_amount: int = 1
-func _on_area_entered(area: Area2D):
-	print("DEBUG: Player sword hit: ", area.name)
-	if area.has_method("take_damage"):
-		area.take_damage(damage_amount, global_position)
+
+@export var damage: int = 2
+
+func _on_body_entered(body: Node2D):
+	# The Player Root must be in group "player"
+	if body.is_in_group("player"):
+		if body.has_method("take_damage"):
+			body.take_damage(damage)
